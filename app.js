3de5b20d6d003a272259a3c0fbc320904ba9393f
mikel39 <202028875+mikel39@users.noreply.github.com>
2025-10-06 17:07:07 -0500

    index html slyling

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
 app.use("/", index);
 app.use("/new", addnew);
 app.use("/edit", update);
